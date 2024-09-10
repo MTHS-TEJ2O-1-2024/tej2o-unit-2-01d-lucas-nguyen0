4cf7d1903d7dda704fe7dd6fff66a33d835c9367
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-09-10 15:21:27 +0000

    changed comment in python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Lucas Nguyen
+Created on: Sep 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Hello, Lucas!")
